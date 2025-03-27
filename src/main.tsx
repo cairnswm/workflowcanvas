be79a73e0c90e9a91e1958d1c951ae8fca9592a0
William Cairns <cairnswm@gmail.com>
2025-03-27 08:37:15 +0200

    Minor Fixes

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,20 @@ import { accessElf } from "./utils/accessElf";
 
 const getEnvVar = (key: string): string | undefined => {
   try {
+    console.log("GETTING ENV VAR", key);
+    console.log("IMPORT META", import.meta.env);
     return import.meta?.env?.[key] as string | undefined;
   } catch {
     // Handle environments where import.meta is not available (like tests)
+    console.log("FALLING BACK TO PROCESS ENV");
     return process.env?.[key] as string | undefined;
   }
 };
 
 const apiKey = getEnvVar("VITE_ACCESSELF_API_KEY");
+console.log("APIKEY", apiKey);
 if (apiKey) {
+  console.log("Setting API Key");
   accessElf.setApiKey(apiKey);
 }
 
